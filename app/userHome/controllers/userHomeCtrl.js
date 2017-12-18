fe3d90bff98af516c67afcff49959056f41b01aa
Paul Ellis <tynesellis@gmail.com>
2017-12-18 16:22:17 -0600

    paginating

--- app/userHome/controllers/userHomeCtrl.js
+++ app/userHome/controllers/userHomeCtrl.js
@@ -77,11 +77,20 @@ angular.module("fwmApp")
         }
         $scope.pages = 0;
         $scope.page = 0;
+        $scope.lastPage = false;
+        $scope.firstPage = true;
+
         $scope.changePage = (direction) => {
             if (direction === "next" && $scope.page < ($scope.pages -1)) {
                 $scope.page += 1;
             } else if (direction === "back" && $scope.page !== 0)
             {$scope.page -= 1}
+            if (($scope.pages -1) === $scope.page) {
+                $scope.lastPage = true
+            }
+            else {$scope.lastPage = false}
+            if ($scope.page !== 0) {$scope.firstPage = false}
+            else {$scope.firstPage = true}
         }
         //array of orders that match the user
         $scope.myOrders = [];
