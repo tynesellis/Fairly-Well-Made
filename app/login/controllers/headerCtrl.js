9cf9be8cede657298dd00de0cf1a7abac099b049
Paul Ellis <tynesellis@gmail.com>
2018-01-22 13:18:48 -0600

    commenting

--- app/login/controllers/headerCtrl.js
+++ app/login/controllers/headerCtrl.js
@@ -1,8 +1,11 @@
 angular.module("fwmApp").controller("headerCtrl",
     function ($scope, UsersFactory, $location) {
-        $scope.loggedIn = false
+        //scope to control diplays in header of index.html based on login status
+        $scope.loggedIn = false;
 
+        //When the route changes
         $scope.$on('$routeChangeSuccess', function () {
+            //if the user has completed registragion or login, set value to affect ng-ifs in index.html
             if ($location.url() === "/userHome") {
                 $scope.loggedIn = true;
             }
